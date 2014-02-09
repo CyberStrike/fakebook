f1b91e89dea2b3908d5fc66c13aded1c5859bede
Chris Scott <Chris@gnmlbs.com>
2014-02-09 09:57:22 -0500

    added status content and user id presence validation and tests.

--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
-class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  class StatusTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+
+    test "Status requires content" do
+      status = Status.new
+      assert ! status.save
+      assert ! status.errors[:content].empty?
+    end
+
+    test "Status content cannot be less than two characters" do
+      status = Status.new
+      status.content = "h"
+      assert ! status.save
+      assert ! status.errors[:content].empty?
+    end
+
+    test "Status requires a user" do
+      status = Status.new
+      status.content = "Helloooo Nurse!"
+      assert ! status.save
+      assert ! status.errors[:user_id].empty?
+    end
+  end
